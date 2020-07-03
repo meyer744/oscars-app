e1b990517ee7da14def4387494f213d4878bb3ce
Your Name <jack.meyer@emerson.com>
2020-07-03 15:16:47 -0400

    Updates

---
+++ precache-manifest.cf8273619c20d4d78e41189cdf03b860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb599847f0433825fc19d1a277247923",
+    "url": "/oscars-app/index.html"
+  },
+  {
+    "revision": "f415a400138ad3427d07",
+    "url": "/oscars-app/static/css/main.e93a50b8.chunk.css"
+  },
+  {
+    "revision": "3a3e4818311b8bea8d7c",
+    "url": "/oscars-app/static/js/2.ff42326e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oscars-app/static/js/2.ff42326e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f415a400138ad3427d07",
+    "url": "/oscars-app/static/js/main.b53aed71.chunk.js"
+  },
+  {
+    "revision": "bc606a88fb77fea29c63",
+    "url": "/oscars-app/static/js/runtime-main.35f24779.js"
+  }
+]);
